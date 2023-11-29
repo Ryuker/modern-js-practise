7030ab9b3ae8fdac9368007dd456b0a0a9f30e14
ryuker <lifeisagamenotaproduct@gmail.com>
2023-11-29 19:47:23 +0100

    added primitive datatypes

---
+++ 01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,10 @@
+// Primitive Data Types
+// ---------------------
+// String | sequence of characters, must be in quotes or backticks
+// Number | Integers as well as floating-point numbers
+// Boolean| Logical entity / true or false
+// Null   | Intentional absense of any object value
+// Undefined | A variable that has not yet been defined / assigned
+// Symbol | Built-in object whose construcor returns a unique symbol
+// BigInt | Numbers that are greater than the "Number" type can handle
+

6f1a5b5277211873c8a993c4d3e07044a641a595
ryuker <lifeisagamenotaproduct@gmail.com>
2023-11-29 17:00:08 +0100

    added comments and keyboard shorts to 02-comments

---
+++ 01-variables-data-types/02-comments/script.js
@@ -0,0 +1,21 @@
+// This is a single line of code comment
+
+/*
+  this is a multine comment
+  this is a multine comment
+*/
+
+// cmd+shift+L | highlights all occurences, you can then edit them
+// cmd+g | add the next occurence to the highlights
+// alt + left / right arrow skips to the next word
+// cmd + left/right arrow skips to the end/beginnen of the line
+// alt + shift + left/right arrow adds/contracts words from the selection 
+// you can click with al to select multiple words to edit in one key stroke
+
+
+
+
+
+
+
+

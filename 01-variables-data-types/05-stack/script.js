e56a9ad5fd96fe8845a6d7be72f233782e8f5c5b
ryuker <lifeisagamenotaproduct@gmail.com>
2023-11-30 18:06:02 +0100

    added reference explorations to 05-stack

--- 01-variables-data-types/05-stack/script.js
+++ 01-variables-data-types/05-stack/script.js
@@ -6,7 +6,23 @@
 // Reference Types: stored in the heap and accessed by reference
 // Arrays | Functions | Objects
 
+// Values are stored on the stack
+const name = 'John';
+const age = 30;
 
+// Reference values are stored on the heap
+const person =  {
+  name: 'Brad'
+};
+
+let newName = name;
+newName = 'Jonathan';
+
+let newPerson = person;
+newPerson.name = 'Bradley';
+
+console.log(name, newName);
+console.log(person, newPerson);
 
 
 

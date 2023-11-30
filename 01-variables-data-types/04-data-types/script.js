33fe415e23e4c6a1906643a02c017b64803b105a
ryuker <lifeisagamenotaproduct@gmail.com>
2023-11-30 17:46:52 +0100

    added type explorations to 04-data-types

--- 01-variables-data-types/04-data-types/script.js
+++ 01-variables-data-types/04-data-types/script.js
@@ -22,7 +22,44 @@
 
 // ---------------------------------------------
 
-// string
+// String
+const firstName = 'Sara';
+// Number
+const age = 30;
+const temp = 98.9;
+
+// Boolean
+const hasKids = true;
+
+// Null
+const aptNumber = null;
+
+// undefined
+// let score;
+const score = undefined;
+
+// Symbol
+const id = Symbol('id');
+
+// BigInt
+const n = 900029293039272837n;
+
+// Reference Types
+const numbers = [1, 2, 3, 4];
+
+const person = {
+  name: 'Brad'
+};
+
+function sayHello(){
+  console.log('Hello');
+}
+
+const output = sayHello;
+
+console.log(output, typeof output);
+
+
 
 
 

b126ac0374a990b8fcd2cc184601325f7d738288
ryuker <lifeisagamenotaproduct@gmail.com>
2023-11-30 17:34:31 +0100

    added type explanations to 04-data-types

--- 01-variables-data-types/04-data-types/script.js
+++ 01-variables-data-types/04-data-types/script.js
@@ -8,3 +8,22 @@
 // Symbol | Built-in object whose construcor returns a unique symbol
 // BigInt | Numbers that are greater than the "Number" type can handle
 
+// Reference Types (Objects)
+// -------------------------
+// Reference types or "objects" are non-primitive value and when assigned to a variable, the variable is given a reference to that value.
+// Object literals, arrays and functions are all reference types.
+
+// Static Typing vs Dynamic Types
+// -------------------------
+// Javascript is a dynamically-types language. 
+// - we do not explicityly define the types for our variables.
+// many other languages are statically typed (c, c++ and Java)
+// Typescript is a superset of JavaScript, whic allow static typing.
+
+// ---------------------------------------------
+
+// string
+
+
+
+

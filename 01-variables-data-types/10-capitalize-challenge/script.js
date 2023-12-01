a47a232d037e4121610a7a248f4e48d500187959
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-01 13:59:46 +0100

    added video solution to 10-capitalize-challenge

--- 01-variables-data-types/10-capitalize-challenge/script.js
+++ 01-variables-data-types/10-capitalize-challenge/script.js
@@ -3,4 +3,20 @@ let x;
 
 x = myString[0].toUpperCase() + myString.slice(1);
 
-console.log(x);
+console.log(x); // 'Developer'
+
+// solution from video
+const str = 'developer';
+let myNewString;
+
+// Solution 1
+myNewString = str.charAt(0).toUpperCase() + myString.substring(1);
+
+// Solution 2
+myNewString = str[0].toUpperCase() + myString.substring(1);
+
+// Solution 2
+myNewString = `${str[0].toUpperCase()}${myString.slice(1)}`;
+
+
+console.log(myNewString);

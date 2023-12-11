d7d8a2854bd50faf097dbb9e7fd6fb3219272058
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-11 16:04:34 +0100

    added date explorations to 14-dates-and-times

---
+++ 01-variables-data-types/14-dates-and-times/script.js
@@ -0,0 +1,18 @@
+let d;
+
+d = new Date();
+
+d = d.toString();
+
+d = new Date(2021, 6, 10);
+
+d = new Date('2021-07-10');
+d = new Date('2021-07-10T12:30:00');
+d = new Date('07/10/2021 12:30:00');
+d = new Date('07/10/2021 12:30:00');
+
+
+
+
+
+console.log(d);

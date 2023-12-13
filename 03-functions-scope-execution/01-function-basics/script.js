0807737f132b5c1aa84b31576d7ca3a9112ab31b
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-13 16:18:03 +0100

    changed folder 01-function-basics, added function basic explorations

---
+++ 03-functions-scope-execution/01-function-basics/script.js
@@ -0,0 +1,25 @@
+// declaring/defining a function
+function sayHello() {
+  console.log('Hello World');
+}
+
+// calling/invoking a function
+sayHello();
+
+// passing data into an function
+function add(num1, num2){
+  console.log(num1 + num2);
+}
+
+add(5, 10);
+
+// returing a value from a function
+function subtract(num1, num2){
+  return num1 - num2;
+} 
+
+const result = subtract(10, 2);
+console.log(result);
+
+
+

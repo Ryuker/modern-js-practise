bdaf1d81552f837385798a4ff889f3b6fe48f4a9
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-14 16:20:37 +0100

    added challenge 1 solution

---
+++ 03-functions-scope-execution/08-function-challenges/script.js
@@ -0,0 +1,12 @@
+// Challenge 1: 
+// - Create a function called getCelcius() that takes a temperature in fahrenheit as an argument and returns the temperature in celcius
+// - for bonus points, write it as a one line arrow function
+
+console.log('Challenge 1: Fahrenheit to Celcius')
+const getCelcius = fahrenheit => (fahrenheit - 32) / 1.8;
+
+console.log(getCelcius(32), 'Celcius');
+console.log(`The temperature is ${getCelcius(32)} \xB0C`);
+
+// Challenge 2: 
+

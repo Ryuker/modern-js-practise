83161aacdf0379df1b9a27ee5a6af9b221acd18f
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-14 15:57:01 +0100

    added IIFE explorations to 07-iife

--- 03-functions-scope-execution/07-iife/script.js
+++ 03-functions-scope-execution/07-iife/script.js
@@ -1,1 +1,21 @@
-// IIFE
+// IIFE - Immediately Invoked Function Expression
+// - used to avoid global scope polution
+
+
+(function ()  {
+  const user = 'John';
+  console.log(user);
+
+  const hello = () => console.log('Hello from the IIFE');
+  hello();
+})(); 
+
+(function(name) {
+  console.log('Hello ' + name);
+})('Shawn');
+
+(function hello() {
+  console.log('Hello');
+})();
+
+

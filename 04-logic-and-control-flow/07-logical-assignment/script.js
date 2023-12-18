503307f330b72eeb86e40265b1d8d64772c44509
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-18 16:53:06 +0100

    added assignment operators explorations to 07-logical-assignment

--- 04-logic-and-control-flow/07-logical-assignment/script.js
+++ 04-logic-and-control-flow/07-logical-assignment/script.js
@@ -1,5 +1,46 @@
-// || - assigns the righ side value only if the left is a falsy value
+// ||= - assigns the righ side value only if the left is a falsy value
 
-// && - assigns the right side value only if the left is a truthy value
+let a = false;
 
-// ?? - assigns the right side value only if the left is null or undefined 
+if (!a) {
+  a = 10;
+}
+
+// shorthand version
+a = a || 10;
+
+// shorter version
+a ||= 10;
+
+console.log(a);
+
+// &&= - assigns the right side value only if the left is a truthy value
+let b = 10;
+
+if (b) {
+  b = 20;
+}
+
+// shorthand
+b = b && 20;
+
+// shortest version
+b &&= 20; 
+
+console.log(b);
+
+// ??= - assigns the right side value only if the left is null or undefined 
+let c = null;
+
+if (c === null || c === undefined) {
+  c = 30;
+}
+
+// shorthand
+c = c ?? 30;
+
+// shortest version
+c ??= 30;
+
+console.log(c);
+

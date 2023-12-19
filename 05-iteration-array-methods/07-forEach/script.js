b4785a65d0702e0ea54e0c9d9e41bdd3e8362e70
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-19 17:55:18 +0100

    added forEach explorations to 07-forEach

--- 05-iteration-array-methods/07-forEach/script.js
+++ 05-iteration-array-methods/07-forEach/script.js
@@ -1,1 +1,38 @@
 // .forEach is a high level Array method
+const socials = ['Twitter', 'LinkedIn', 'Facebook', 'Instagram'];
+
+// console.log(social.__proto__);
+
+// long version
+console.log('Long version');
+socials.forEach(function(item) {
+  console.log(item);
+});
+
+// shorthand
+console.log('Shorthand version');
+socials.forEach(item => console.log(item));
+
+// other arguments we can pass in
+console.log('Other arguments - item, index, arr');
+socials.forEach((item, index, arr) => console.log(`${index} - ${item}`, arr));
+
+// Passing in a function
+console.log('Passing in a function'); 
+function logSocials(social) {
+  console.log(social);
+}
+
+socials.forEach(logSocials);
+
+// forEach on an object array
+console.log('forEach on an object array');
+const socialObjs = [
+  { name: 'Twitter', url: 'https://twitter.com'},
+  { name: 'Facebook', url: 'https://facebook.com'},
+  { name: 'LinkedIn', url: 'https://linkedin.com'},
+  { name: 'Instagram', url: 'https://instagram.com'}
+];
+
+// socialObjs.forEach();
+socialObjs.forEach( item => console.log(item.url));

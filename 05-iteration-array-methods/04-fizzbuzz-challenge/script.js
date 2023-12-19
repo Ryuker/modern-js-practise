28b1f1d39426c373fd1a0a950f9f7b3edc44cc34
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-19 14:50:22 +0100

    added video solution to 04-fizzbuzz-challenge

--- 05-iteration-array-methods/04-fizzbuzz-challenge/script.js
+++ 05-iteration-array-methods/04-fizzbuzz-challenge/script.js
@@ -1,22 +1,54 @@
-// Fizzbuzz Challenge
+// FizzBuzz Challenge
 // - print/log the numbers from 1 to 100
 // - for 'multiples of three' print "Fizz" instead of the number
 // - for 'multiples of five' print 'Buzz'
 // - For numbers which are 'multiples of both three and five' print 'FizzBuzz'
 
+// My Solution
+console.log('My Solution')
 console.log(15 % 3);
 
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0 ) {
     console.log('FizzBuzz - ' + i );
-    continue;
   }else if (i % 3 === 0 ) {
     console.log('Fizz - ' + i);
-    continue;
-  } else if (i % 5 == 0 ) {
+  } else if (i % 5 === 0 ) {
     console.log('Buzz - '  + i);
-    continue;
   } else {
     console.log(i);
   }
+}
+
+// Video Solution
+console.log('Video Solution - For version');
+for (let i = 1; i <= 100; i++) {
+
+  if (i % 15 === 0) {
+    console.log('FizzBuzz');
+  } else if (i % 3 === 0) {
+    console.log('Fizz');
+  } else if(i % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
+  }
+}
+
+// While loop version
+console.log('Video solution - While version')
+let j = 1;
+while(j <= 100) {
+
+  if (j % 15 === 0) {
+    console.log('FizzBuzz');
+  } else if (j % 3 === 0) {
+    console.log('Fizz');
+  } else if(j % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(j);
+  }
+
+  j++;
 }

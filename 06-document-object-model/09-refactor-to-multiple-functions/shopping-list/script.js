3e4fac6c248939d02ab11dbf4a89ea8b40c00c8d
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-22 14:45:34 +0100

    added refactored exploration to 09-refactor-to-multiple-functions

--- 06-document-object-model/09-refactor-to-multiple-functions/shopping-list/script.js
+++ 06-document-object-model/09-refactor-to-multiple-functions/shopping-list/script.js
@@ -1,45 +1,40 @@
-// My Solution
-
-
-
-// Quick & Dirty
-function createListItem(item){
-  const li = document.createElement('li');
-
-  li.innerHTML = `${item}
-    <button class="remove-item btn-link text-red">
-      <i class="fa-solid fa-xmark"></i>
-    </button>`;
-
-  document.querySelector('.items').appendChild(li);
-}
-
-createListItem('Eggs');
-
-
 // Clean & Performant 
 function createNewItem(item) {
   console.log(`creating new item ${item}`);
 
   // initialization of elements
   const li = document.createElement('li');
-  const button = document.createElement('button');
-  const icon = document.createElement('i');
-
-  // add classes to the button and i element
-  button.className = 'remove-item btn-link text-red'; 
-
-  icon.className = 'fa-solid fa-xmark';
-  button.appendChild(icon);  // append the i element to the button
-
   li.appendChild(document.createTextNode(item));
+  const button = createButton('remove-item btn-link text-red');
+
   li.appendChild(button); // append the button to the li item
 
   // add li item it to the back of the list
   document.querySelector('.items').appendChild(li); 
 }
 
+
+/////////////
+// Functions
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);  // append the i element to the button
+
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  
+  return icon;
+}
+
 createNewItem('Cheese');
+createNewItem('Sauce');
 
 
 

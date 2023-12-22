1530a91e203446dde8d8212c00e4e1a905c6063d
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-22 15:14:20 +0100

    added insert elements exploration to 10-insert-elements

--- 06-document-object-model/10-insert-elements/shopping-list/script.js
+++ 06-document-object-model/10-insert-elements/shopping-list/script.js
@@ -1,10 +1,46 @@
 // insertAdjacentElement Example
+function insertElement() {
+  const filter = document.querySelector('.filter');
+
+  const h1 = document.createElement('h1');
+  h1.textContent = 'insertAdjacentElement';
+
+  filter.insertAdjacentElement('afterend', h1);
+}
+
+insertElement();
 
 // insertAdjacentText Example
+function insertText() {
+  const item = document.querySelector('li:first-child');
+
+  item.insertAdjacentText('beforebegin', 'insertAdjacentText');
+}
+
+insertText();
 
 // insertAdjacentHTML Example
+function insertHTML() {
+  const clearBtn = document.querySelector('#clear');
+
+  clearBtn.insertAdjacentHTML('afterend', '<h2>insertAdjacentHTML</h2>');
+}
+
+insertHTML();
 
 // insertBefore Example
+function insertBeforeItem() {
+  const ul = document.querySelector('ul');
+
+  const li = document.createElement('li');
+  li.textContent = 'insertBefore';
+
+  const thirdItem = document.querySelector('li:nth-child(3)');
+  
+  ul.insertBefore(li, thirdItem); // you call this on the parent
+}
+
+insertBeforeItem();
 
 /*
 <!-- beforeBegin -->

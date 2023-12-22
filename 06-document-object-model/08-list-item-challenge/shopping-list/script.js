0ecab1ced568c08f894687c75b2b31690627f01b
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-22 14:35:53 +0100

    added Clean and Performant method of creating list item to 08-list-item-challenge

--- 06-document-object-model/08-list-item-challenge/shopping-list/script.js
+++ 06-document-object-model/08-list-item-challenge/shopping-list/script.js
@@ -17,12 +17,29 @@ function createListItem(item){
 createListItem('Eggs');
 
 
-// Clean & Performant
+// Clean & Performant 
 function createNewItem(item) {
-  console.log('creating new item');
+  console.log(`creating new item ${item}`);
+
+  // initialization of elements
+  const li = document.createElement('li');
+  const button = document.createElement('button');
+  const icon = document.createElement('i');
+
+  // add classes to the button and i element
+  button.className = 'remove-item btn-link text-red'; 
+
+  icon.className = 'fa-solid fa-xmark';
+  button.appendChild(icon);  // append the i element to the button
+
+  li.appendChild(document.createTextNode(item));
+  li.appendChild(button); // append the button to the li item
+
+  // add li item it to the back of the list
+  document.querySelector('.items').appendChild(li); 
 }
 
-createNewItem();
+createNewItem('Cheese');
 
 
 

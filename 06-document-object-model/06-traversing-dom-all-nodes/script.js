e0afba71c1e57c9e95644c421cdf925096ab55a8
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-22 13:42:13 +0100

    added node type explorations to 06-traversing-dom-all-nodes

--- 06-document-object-model/06-traversing-dom-all-nodes/script.js
+++ 06-document-object-model/06-traversing-dom-all-nodes/script.js
@@ -34,4 +34,41 @@
 // 12. Notation: represents a notation declared in the DTD
 //    children: None
 
+// ---------------------------
+
+let output;
+const parent = document.querySelector('.parent');
+
+output = parent.childNodes; // contains a NodeList (including white space txt)
+
+output = parent.childNodes[1].textContent;
+output = parent.childNodes[3].nodeName;
+output = parent.childNodes[3].textContent;
+output = parent.childNodes[3].innerHTML;
+output = parent.childNodes[3].outerHTML;
+
+output = parent.childNodes[3].innerText = 'Child One'; 
+output = parent.childNodes[5].style.color = 'red'; 
+
+output = parent.firstChild;
+output = parent.lastChild;
+
+parent.lastChild.textContent = 'Hello';
+
+// Parent node
+const child = document.querySelector('.child');
+
+output = child.parentNode;
+output = child.parentElement;
+
+child.parentNode.style.backgroundColor = '#ccc';
+child.parentNode.style.padding = '10px';
+
+// Siblings
+const secondItem = document.querySelector('.child:nth-child(2)');
+
+output = secondItem.nextSibling;
+output = secondItem.previousSibling;
+
+console.log(output);
 

900b7eed6d16a11b829740fcb188ce26d6d1d8e9
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-27 22:42:09 +0100

    added keyboard event explorations

--- 07-events/04-keyboard-events/shopping-list/script.js
+++ 07-events/04-keyboard-events/shopping-list/script.js
@@ -1,1 +1,28 @@
 // Keyboard Events
+const itemInput = document.getElementById('item-input');
+
+
+
+// key press - fires on single press
+const onKeyPress = e => {
+  console.log('key: ' + e.key); // logs the key pressed
+};
+
+// key up - fires when released
+const onKeyUp = e => {
+  console.log('key: ' + e.key); // logs the key pressed
+};
+
+// key down - fires when key is held down
+const onKeyDown = e => {
+  console.log('key: ' + e.key); // logs the key pressed
+};
+
+
+
+
+
+
+// itemInput.addEventListener('keypress', onKeyPress); // keypress event
+// itemInput.addEventListener('keyup', onKeyUp); // keyup event
+itemInput.addEventListener('keydown', onKeyDown); // keydown event

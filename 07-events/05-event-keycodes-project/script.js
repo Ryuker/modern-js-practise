cdfa2eaf9c8ab3d1902b7c82b649671ca31964bf
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-28 15:14:08 +0100

    added my solution and video solution to 05-event-keycodes-project

--- 07-events/05-event-keycodes-project/script.js
+++ 07-events/05-event-keycodes-project/script.js
@@ -1,1 +1,69 @@
 // Event Key Codes Project
+const keys = document.querySelectorAll('.key');
+
+function onKeyPress(e) {
+  // console.log('key pressed');
+  
+  // console.dir(keys[0]);
+
+  // keys[0].firstChild.innerText = e.key.toString();
+  
+  // keys[1].querySelector('small').innerText = e.keyCode;
+  
+  // keys[2].querySelector('small').innerText = e.code;
+}
+
+window.addEventListener('keydown', onKeyPress);
+
+
+// video solution
+// method 1
+// window.addEventListener('keydown', (e) => {
+//   const insert = document.getElementById('insert');
+//   insert.innerHTML = `
+//     <div class="key">
+//       ${e.key === ' ' ? 'Space' : e.key}
+//       <small>e.key</small>
+//     </div>
+
+//     <div class="key">
+//       ${e.keyCode}
+//       <small>e.keyCode</small>
+//     </div>
+    
+//     <div class="key">
+//       ${e.code}
+//       <small>e.code</small>
+//     </div>
+//   `;
+
+// });
+
+// method 2
+window.addEventListener('keydown', (e) => {
+  const insert = document.getElementById('insert');
+  insert.innerHTML = '';
+
+  const keyCodes = {
+    'e.key': e.key === ' ' ? 'Space' : e.key,
+    'e.keyCode': e.keyCode,
+    'e.code': e.code,
+  };
+
+  console.log(keyCodes);
+
+  for(let key in keyCodes) {
+    const div = document.createElement('div');
+    div.className = 'key';
+    const small = document.createElement('small');
+
+    const keyText = document.createTextNode(key);
+    const valueText = document.createTextNode(keyCodes[key]);
+
+    small.appendChild(keyText);
+    div.appendChild(valueText);
+    div.appendChild(small);
+
+    insert.appendChild(div);
+  }
+});

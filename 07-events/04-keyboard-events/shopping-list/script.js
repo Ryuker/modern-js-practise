4a6f12d45dcb9ea767555297429b83eb19672686
ryuker <lifeisagamenotaproduct@gmail.com>
2023-12-28 14:18:04 +0100

    added keyboard event explorations to 04-keyboard-events

--- 07-events/04-keyboard-events/shopping-list/script.js
+++ 07-events/04-keyboard-events/shopping-list/script.js
@@ -1,5 +1,8 @@
 // Keyboard Events
+// keycodes:
+// https://www.toptotal.com/developers/keycode/table-of-all-keycodes
 const itemInput = document.getElementById('item-input');
+
 
 
 
@@ -16,7 +19,36 @@ const onKeyUp = e => {
 // key down - fires when key is held down
 const onKeyDown = e => {
   console.log('key: ' + e.key); // logs the key pressed
+
+  // Key - is a letter
+  console.log(e.key);
+  if (e.key === 'enter')
+    console.log('You pressed enter');
+  
+  // KeyCode - is a number
+  if (e.keyCode === 13)
+    console.log('You pressed enter');
+  
+  // code - digit code
+  if (e.code === 'Digit1')
+    console.log('You pressed 1');
+
+  // register if specific key is being held down
+  if (e.repeat) {
+    console.log('You are holding down ' + e.key);
+  }
+
+  // check for key combinations held down
+  console.log('Shift: ' + e.shiftKey);
+  console.log('Control: ' + e.ctrlKey);
+  console.log('Alt: ' + e.altKey);
+
+  // conditional if key combination is held down
+  if (e.shiftKey && e.key === 'K')
+    console.log('You hit Shift + K');
 };
+
+
 
 
 

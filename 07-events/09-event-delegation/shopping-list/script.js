9106feb5b42a5cd2e256ecafe634c4f1ab10e22d
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-02 17:38:31 +0100

    added event delegation explorations to 09-event-delegation

--- 07-events/09-event-delegation/shopping-list/script.js
+++ 07-events/09-event-delegation/shopping-list/script.js
@@ -1,1 +1,28 @@
 // Event Delegation
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// inefficient approach
+// listItems.forEach(item => {
+//   item.addEventListener('click', (e) => {
+//     console.dir(e.target.outerText);
+    
+//     e.target.remove(); // remove the list item
+//   })
+// });
+
+// event delegation approach
+list.addEventListener('click', e => {
+  console.log(e.target);
+  if (e.target.tagName === 'LI') {
+    e.target.remove();
+  }
+});
+
+// mouse over
+list.addEventListener('mouseover', e => {
+  console.log(e.target);
+  if (e.target.tagName === 'LI') {
+    e.target.style.color = 'red';
+  }
+});

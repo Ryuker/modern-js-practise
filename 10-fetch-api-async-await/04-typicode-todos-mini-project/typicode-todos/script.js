b8f20ec5d91bc2e957d87fd6268e78dfc6416076
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-09 22:45:43 +0100

    added functionality to getToDos from API, added functionality to mock add a new ToDo to the api

--- 10-fetch-api-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-api-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -1,1 +1,54 @@
 // Typicode Todos Mini Project
+// api: https://jsonplaceholder.typicode.com/todos
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+const getToDos = () => {
+  console.log('Get the Todos from the API');
+  fetch(apiUrl + '?_limit=5')
+  .then(res => res.json())
+  .then(data => data.forEach(todo => addTodoToDOM(todo)));
+};
+
+const createTodo = (e) => {
+  e.preventDefault();
+
+  const newTodo = {
+    title: e.target.firstElementChild.value,
+    completed: false
+  };
+
+  fetch(apiUrl, {
+    method: 'POST',
+    body: JSON.stringify(newTodo),
+    headers: {
+      'Content-Type': 'application/json',
+      token: 'abc123',
+    }
+  })
+    .then(res => res.json())
+    .then(data => addTodoToDOM(data));
+}
+
+
+const addTodoToDOM = (todo) => {
+  const div = document.createElement('div');
+  div.appendChild(document.createTextNode(todo.title));
+  div.setAttribute('data-id', todo.id);
+
+  // color completed todos done
+  if (todo.completed) {
+    div.classList.add('done');
+  }
+
+  document.getElementById('todo-list').appendChild(div);
+}
+
+const init = () => {
+  document.addEventListener('DOMContentLoaded', getToDos);
+  document.querySelector('#todo-form').addEventListener('submit', createTodo);
+};
+
+init();
+
+
+

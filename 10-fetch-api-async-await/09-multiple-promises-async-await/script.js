b70d2bc2785733488493b145ce3835633bde638b
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-10 00:33:23 +0100

    added async approach to await multiple promises before logging their data

--- 10-fetch-api-async-await/09-multiple-promises-async-await/script.js
+++ 10-fetch-api-async-await/09-multiple-promises-async-await/script.js
@@ -1,38 +1,51 @@
 //Multiple Promises with Async / Await
 
-function getData(endpoint) {
-  return new Promise((resolve, reject) => {
+// origincal xhr example to get the data
+// function getData(endpoint) {
+//   return new Promise((resolve, reject) => {
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', endpoint);
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', endpoint);
 
-    xhr.onreadystatechange = function () {
-      if (this.readyState === 4) {
-        if (this.status === 200) {
-          resolve(JSON.parse(this.responseText));
-        } else {
-          reject('Error: something went wrong');
-        }
-      }
-    }
+//     xhr.onreadystatechange = function () {
+//       if (this.readyState === 4) {
+//         if (this.status === 200) {
+//           resolve(JSON.parse(this.responseText));
+//         } else {
+//           reject('Error: something went wrong');
+//         }
+//       }
+//     }
 
-    setTimeout(() => {
-      xhr.send();
-    }, Math.floor(Math.random() * 3000 + 1000));
+//     setTimeout(() => {
+//       xhr.send();
+//     }, Math.floor(Math.random() * 3000 + 1000));
 
-  });
-}
+//   });
+// }
 
-getData('./movies.json')
-  .then((movies) => {
-    console.log(movies);
-    return getData('./actors.json');
-  })
-  .then((actors) =>  {
-    console.log(actors);
-    return getData('./directors.json');
-  })
-  .then((directors) => {
-    console.log(directors);
-  })
-  .catch((error) => console.log(error));
+// original promise chain to get the data
+// getData('./movies.json')
+//   .then((movies) => {
+//     console.log(movies);
+//     return getData('./actors.json');
+//   })
+//   .then((actors) =>  {
+//     console.log(actors);
+//     return getData('./directors.json');
+//   })
+//   .then((directors) => {
+//     console.log(directors);
+//   })
+//   .catch((error) => console.log(error));
+
+// Async approach to get the data
+async function getAllData() {
+  const movies = await getData('./movies.json');
+  const actors = await getData('./actors.json');
+  const directors = await getData('./directors.json');
+  console.log(movies, actors, directors);
+  
+} 
+
+getAllData();

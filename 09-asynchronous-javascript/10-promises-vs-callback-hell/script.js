e2c5bd7917565f70bd65b348888b1efc6fe4eef2
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-09 01:24:53 +0100

    added callback to promises refactor to fix callback hell

--- 09-asynchronous-javascript/10-promises-vs-callback-hell/script.js
+++ 09-asynchronous-javascript/10-promises-vs-callback-hell/script.js
@@ -1,32 +1,44 @@
 // Promises vs Callback Hell
 // Callback Hell
 
-function getData(endpoint, cb) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', endpoint);
+function getData(endpoint) {
+  return new Promise((resolve, reject) => {
 
-  xhr.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      cb(JSON.parse(this.responseText));
-      
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', endpoint);
+
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          resolve(JSON.parse(this.responseText));
+        } else {
+          reject('Error: something went wrong');
+        }
+      }
     }
-  }
 
-  setTimeout(() => {
-    xhr.send();
-  }, Math.floor(Math.random() * 3000 + 1000));
+    setTimeout(() => {
+      xhr.send();
+    }, Math.floor(Math.random() * 3000 + 1000));
+
+  });
 }
 
 // This gets really messy and this is why there are promises
-getData('./movies.json', (data) => {
-  console.log(data);
-  getData('./actors.json', (data) => {
-    console.log(data)
-    getData('./directors.json', (data) => {
-      console.log(data);
-    });
-  });
-});
 
-// getData('./actors.json');
-// getData('./directors.json');
+getData('./movies.json')
+  .then((movies) => {
+    console.log(movies);
+    return getData('./actors.json');
+  })
+  .then((actors) =>  {
+    console.log(actors);
+    return getData('./directors.json');
+  })
+  .then((directors) => {
+    console.log(directors);
+  })
+  .catch((error) => console.log(error));
+
+
+  // getData('./directors.json');

c182ccd8627c181a1e57c6eca75088cdefe340bb
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-09 01:33:53 +0100

    added promise.all explorations

--- 09-asynchronous-javascript/11-promise-all/script.js
+++ 09-asynchronous-javascript/11-promise-all/script.js
@@ -23,16 +23,19 @@ function getData(endpoint) {
   });
 }
 
-getData('./movies.json')
-  .then((movies) => {
-    console.log(movies);
-    return getData('./actors.json');
-  })
-  .then((actors) =>  {
-    console.log(actors);
-    return getData('./directors.json');
-  })
-  .then((directors) => {
-    console.log(directors);
-  })
-  .catch((error) => console.log(error));
+const moviesPromise = getData('./movies.json');
+const actorsPromise = getData('./actors.json');
+const directorsPromise = getData('./directors.json');
+
+const dummyPromise = new Promise((resolve, reject) => {
+  resolve('Hello world');
+});
+
+// returns an array of the promise results
+Promise.all([moviesPromise, actorsPromise, directorsPromise, dummyPromise])
+  .then((data) => {
+    console.log(data);
+  }).catch((error) => {
+    console.log(error);
+  });
+

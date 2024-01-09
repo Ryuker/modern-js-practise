fbd3da5ced1066a14a2142a1f11acb1e98e6ad06
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-09 18:15:10 +0100

    added random-user-generator functionality

--- 10-fetch-api-async-await/02-random-user-project/random-user-generator/script.js
+++ 10-fetch-api-async-await/02-random-user-project/random-user-generator/script.js
@@ -1,1 +1,63 @@
 // Random User Generator
+// api: https://randomuser.me/api
+
+function fetchUser() {
+  fetch('https://randomuser.me/api')
+  .then(res => res.json())
+  .then(data => { 
+    displayUser(data.results[0]);
+  });
+}
+
+function displayUser(user) {
+  // displays the user
+  console.log(user, 'displaying user');
+  const userDisplay = document.querySelector('#user');
+
+  // change background color based on gender
+  if (user.gender === 'female') {
+    document.body.style.backgroundColor = 'rebeccapurple';
+  } else {
+    document.body.style.backgroundColor = 'steelblue';
+  }
+
+  // Change the userDisplay to display the user
+  userDisplay.innerHTML = `
+    <div class="flex justify-between>
+      <div class="flex">
+        <img
+          class="w-48 h-48 rounded-full mr-8"
+          src="${user.picture.large}"
+        />
+        <div class="space-y-3">
+          <p class="text-xl">
+            <span class="font-bold">Name: </span> 
+            ${user.name.first} ${user.name.last}
+          </p>
+          <p class="text-l">
+            <span class=font-bold">Email: </span> ${user.email}
+          </p>
+          <p class="text-l">
+            <span class=font-bold">Phone: </span> ${user.phone}
+          </p>
+          <p class="text-l">
+            <span class=font-bold">Location: </span> 
+            ${user.location.city} ${user.location.state} | ${user.location.country} 
+          </p>
+          <p class="text-xl">
+            <span class=font-bold">Age: </span> ${user.dob.age}
+          </p>
+        </div
+      </div>
+    </div>
+  `;
+}
+
+document.querySelector('#generate').addEventListener('click', fetchUser);
+fetchUser();
+
+
+
+
+
+

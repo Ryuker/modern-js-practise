5440a680ac31abc3d1cbf5b503d75a201efb51ac
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-10 00:06:39 +0100

    added try-catch explorations

--- 10-fetch-api-async-await/07-try-catch/script.js
+++ 10-fetch-api-async-await/07-try-catch/script.js
@@ -1,1 +1,25 @@
 // Try / Catch Error handling of Async / Await functions
+
+// Catching a 'not defined' error
+try  {
+  console.log(x);
+} catch(error) {
+  console.log('Error: ' + error);
+}
+
+
+// Throwing your own error and catching it
+function double(number) {
+  if (isNaN(number)) {
+    throw new Error(number + ' is not a number');
+  }
+
+  return number * 2;
+}
+
+try {
+  const y = double('bla');
+  console.log(y);
+} catch(error) {
+  console.log(error);
+}

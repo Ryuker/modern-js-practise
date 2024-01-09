0647dc587f014cd83ff2a402bda30f1f846f7a1f
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-10 00:27:41 +0100

    replaced the resolve all with the chaining for the setup

--- 10-fetch-api-async-await/09-multiple-promises-async-await/script.js
+++ 10-fetch-api-async-await/09-multiple-promises-async-await/script.js
@@ -23,19 +23,16 @@ function getData(endpoint) {
   });
 }
 
-const moviesPromise = getData('./movies.json');
-const actorsPromise = getData('./actors.json');
-const directorsPromise = getData('./directors.json');
-
-const dummyPromise = new Promise((resolve, reject) => {
-  resolve('Hello world');
-});
-
-// returns an array of the promise results
-Promise.all([moviesPromise, actorsPromise, directorsPromise, dummyPromise])
-  .then((data) => {
-    console.log(data);
-  }).catch((error) => {
-    console.log(error);
-  });
-
+getData('./movies.json')
+  .then((movies) => {
+    console.log(movies);
+    return getData('./actors.json');
+  })
+  .then((actors) =>  {
+    console.log(actors);
+    return getData('./directors.json');
+  })
+  .then((directors) => {
+    console.log(directors);
+  })
+  .catch((error) => console.log(error));

1b92e8709f52bc71ad91788d25ed8a1973598d6f
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-10 00:18:18 +0100

    added example of try catch in async function. added example of using .catch to handle the error from async function

--- 10-fetch-api-async-await/08-async-await-error-handling/script.js
+++ 10-fetch-api-async-await/08-async-await-error-handling/script.js
@@ -1,1 +1,37 @@
 // Async / Await Error Handling
+
+// Try / Catch
+const getUsers = async () => {
+  try {
+    // const response = await fetch('https://jsonplaceholder.typicode.com/user');
+    const response = await fetch('https://httpstat.us/404');
+    if (!response.ok) {
+      throw new Error('Request Failed');
+    }
+
+    const data = await response.text();
+
+    
+    console.log(data);
+
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+getUsers();
+
+// Using Async Await but catching the error with .catch on the function call
+const getPosts = async () => {
+  // const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const response = await fetch('https://httpstat.us/500');
+  if (!response.ok) {
+    throw new Error('Request Failed');
+  }
+
+  const data = await response.text();
+
+  console.log(data);
+};
+
+getPosts().catch(error => console.log(error));

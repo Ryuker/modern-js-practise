e2c0ddb26ea6f742c610b65b7e7f4a478c6e05a2
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-11 18:39:43 +0100

    removed the cards in popular movies, added createCardInnerHTML() and createCard() - both do the same but the last one does it using sequential createElements, added functionality to display popular movies in the home page

--- 11-flix-app-project/js/script.js
+++ 11-flix-app-project/js/script.js
@@ -2,23 +2,40 @@
 // api: themoviedb.org
 // docs: https://developer.themoviedb.org/docs/getting-started
 
-// ENV imports 
+// API imports 
 import API_KEY from '../../keys/keys.js';
+const API_URL = 'https://api.themoviedb.org/3';
 
 // Page Router
 const root = '/11-flix-app-project';
 const global = { currentPage: window.location.pathname.replace(root, '') } ;
 
-// async function 
+// Display popular movies
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('/movie/popular');
+  console.log(results);
+  console.log(results[0]);
+
+  const movie = results[0];
+
+  const popularMoviesEL = document.getElementById('popular-movies');
+
+  results.forEach(movie => {
+    // create div
+    // const div = createCard(movie);
+    const div = createCardInnerHTML(movie);
+
+    popularMoviesEL.appendChild(div);
+
+  });
+}
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
-  const API_URL = 'https://api.themoviedb.org/3';
-
   console.log('Fetching data from API');
   const response = await fetch(`${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`);
   const data = await response.json();
-  console.log(data);
+  // console.log(data);
   return data;
 }
 
@@ -43,6 +60,7 @@ function init() {
     case `/`:
     case `/index.html`:
       console.log('Home');
+      displayPopularMovies();
       break;
     case `/shows.html`:
       console.log('Shows');
@@ -59,10 +77,94 @@ function init() {
   }
 
   highlightActiveLink();
-  fetchAPIData('/movie/popular');
+  // fetchAPIData('/movie/popular');
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+// create a card with inner HTML instead
+function createCardInnerHTML(movie) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  card.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+      ${
+        movie.poster_path ? `
+          <img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+          />` : `
+            <img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+      }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${movie.title}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${movie.release_date}</small>
+      </p>
+    </div>
+  `;
+
+  return card;
+  
+}
+
+// For a function to compose the movie card line by line
+// Long version with creating seperate elements instead
+function createCard(movie) {
+  const div = document.createElement('div');
+  div.classList.add('card');
+  // create link
+  const a = document.createElement('a');
+  a.href = `/movie-details.html?id=${movie.id}`;
+
+  // create img
+  const img = document.createElement('img');
+  img.src = movie.poster_path 
+      ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` 
+      : 'images/no-image.jpg';
+  img.classList.add('card-img-top');
+  img.alt = movie.title;
+
+  console.log(img);
+
+  // create card body div
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+
+  // create title
+  const cardTitle = document.createElement('h5');
+  cardTitle.classList.add('card-title');
+
+  // create card text
+  const cardText = document.createElement('p')
+  cardText.classList.add('card-text');
+  
+  // create small element for release date
+  const cardRelease = document.createElement('small');
+  cardRelease.classList.add('text-muted');
+  cardRelease.textContent = `Release: ${movie.release_date}`;
+  
+  // Appends of the elements
+  cardText.appendChild(cardRelease);
+  cardBody.appendChild(cardText);
+
+  a.appendChild(img);
+
+  div.appendChild(a);
+  div.appendChild(cardBody);
+  
+  // return the new card;
+  return div;
+}
+    
 
 
 

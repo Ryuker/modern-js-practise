f0c32e1d76b81b890bdf41a45bb0a49e6086331e
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-11 16:56:48 +0100

    added highLightActiveLink to add 'active' class to active nav link

--- 11-flix-app-project/js/script.js
+++ 11-flix-app-project/js/script.js
@@ -3,29 +3,46 @@
 // docs: https://developer.themoviedb.org/docs/getting-started
 
 // Page Router
-const global = { currentPage: window.location.pathname } ;
 const root = '/11-flix-app-project';
+const global = { currentPage: window.location.pathname.replace(root, '') } ;
+
+// Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+
+  links.forEach(link => {
+    const href = link.getAttribute('href');
+
+    if (href === global.currentPage ) {
+      link.classList.add('active');
+    } else if(global.currentPage === '/index.html' && href === '/') {
+      link.classList.add('active');
+    }
+  });
+} 
 
 // Init App
 function init() {
   switch (global.currentPage) {
-    case `${root}/`:
-    case `${root}/index.html`:
+    case `/`:
+    case `/index.html`:
       console.log('Home');
       break;
-    case `${root}/shows.html`:
+    case `/shows.html`:
       console.log('Shows');
       break;
-    case `${root}/movie-details.html`:
+    case `/movie-details.html`:
       console.log('Movie Details');
       break;
-    case `${root}/tv-details.html`:
+    case `/tv-details.html`:
       console.log('TV Details');
       break;
-    case `${root}/search.html`:
+    case `/search.html`:
       console.log('Search');
       break;
   }
+
+  highlightActiveLink();
 }
 
 document.addEventListener('DOMContentLoaded', init);

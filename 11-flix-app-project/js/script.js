17641a08dfe7f6d61d5ff5d18da07b1e63281882
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-12 19:06:50 +0100

    added backdrop functionality for the details pages

--- 11-flix-app-project/js/script.js
+++ 11-flix-app-project/js/script.js
@@ -43,6 +43,9 @@ async function displayMovieDetails () {
   const movieId = window.location.search.split('=')[1];
   const movie = await fetchAPIData(`movie/${movieId}`);
 
+  // Overlay for background image
+  displayBackgroundImage('movie', movie.backdrop_path);
+
   const div = document.createElement('div');
   div.innerHTML = `
     <div class="details-top">
@@ -95,6 +98,33 @@ async function displayMovieDetails () {
   `
   // add the div to the movie details element
   document.querySelector('#movie-details').appendChild(div);
+}
+
+// Display backdrop on details pages
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+
+  // styles
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+
+  // add to the details element
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
+
+  // console.dir(overlayDiv);
 }
 
 // Show Spinner when data is being retrieved

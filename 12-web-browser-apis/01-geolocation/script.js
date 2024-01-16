1be06d2793156d7b6cd144f1ec57bc7efc4232c0
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-16 16:19:26 +0100

    added watchPosition exploration

--- 12-web-browser-apis/01-geolocation/script.js
+++ 12-web-browser-apis/01-geolocation/script.js
@@ -18,18 +18,31 @@ const curOptions = {
   maximumAge: 0             // Do not use a cached position
 };
 
-navigator.geolocation.getCurrentPosition(curSuccess, curError, curOptions);
+// navigator.geolocation.getCurrentPosition(curSuccess, curError, curOptions);
 
-// Watch Position
+// Watch Position - fires of when location changes
+const target = {
+  latitude: 41.387438,
+  longitude: -71.394839
+};
+
 function watchSuccess(pos) {
   const coords = pos.coords;
-  console.log(`Latitude: ${coords.latitude}`);
-  console.log(`Longitude: ${coords.longitude}`);
-  console.log(`Within: ${coords.accuracy} meters`);
+  // console.log(coords);
+  if (target.latitude === coords.latitude && target.longitude === coords.longitude) {
+    console.log('You have reached your destination');
+    navigator.geolocation.clearWatch(id);
+  }
 }
 
 function watchError(err) {
   console.log(`Error: ${err.code} - ${err.message}`);
 }
 
-navigator.geolocation.watchPosition(watchSuccess, watchError, curOptions);
+const watchOptions = {
+  enableHighAccuracy: true, // Use GPS if available
+  timeout: 5000,            // Time to wait to stop trying for location
+  maximumAge: 0             // Do not use a cached position
+};
+
+const id = navigator.geolocation.watchPosition(watchSuccess, watchError, watchOptions);

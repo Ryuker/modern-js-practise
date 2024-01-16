899cfe29cd309f34791a6f9430d0c6513518347b
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-16 17:46:56 +0100

    added requestAnimationFrame exploration with a moving and rotating ball example

--- 12-web-browser-apis/04-requestAnimationFrame/script.js
+++ 12-web-browser-apis/04-requestAnimationFrame/script.js
@@ -1,1 +1,38 @@
 // requestAnimationFrame() exploration
+// ref: https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+
+const image = document.querySelector('img');
+let start;
+let done = false;
+
+
+// We can do animations in here
+// Example of translating the image and rotating it based on the elapsed timestamp
+function step(timestamp) {
+  if (start === undefined) {
+    start = timestamp;
+  }
+
+  const elapsed = timestamp - start;
+  if (elapsed > 5000) {
+    done = true;
+  }
+
+  if (done) {
+    return;
+  }
+
+  image.style.transform = `
+    translateX(${elapsed / 20}px) 
+    rotate(${elapsed / 10}deg)
+  `;
+
+  // call step recursively
+  requestAnimationFrame(step);
+}
+
+// call before the next repaint
+requestAnimationFrame(step);
+
+
+

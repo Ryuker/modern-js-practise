26e4a71eb5cad87804a604ccd23833edbfdd5158
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-17 19:01:04 +0100

    added play and pause functionality

--- 12-web-browser-apis/08-music-player-project/music-player/script.js
+++ 12-web-browser-apis/08-music-player-project/music-player/script.js
@@ -1,1 +1,28 @@
 // Music Player
+const musicContainer = document.getElementById('music-container');
+const audio = document.getElementById('audio');
+const play = document.getElementById('play');
+const playIcon = play.querySelector('i');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+
+// listeners
+
+function togglePlayPause() {
+  // play the music
+  if (audio.paused) {
+    audio.play();
+    // change the icon to display the pause button
+    playIcon.classList.remove('fa-play');
+    playIcon.classList.add('fa-pause');
+  } else {
+    audio.pause();
+
+    // change the icon to display the pause button
+    playIcon.classList.remove('fa-pause');
+    playIcon.classList.add('fa-play');
+  }
+}
+
+play.addEventListener('click', togglePlayPause);

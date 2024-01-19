b268607b4d5dc0a8850e3a026ac984e90dc4c9c3
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-19 16:32:44 +0100

    added functionality to speak out what's written in a text field

--- 12-web-browser-apis/14-speech-synthesis/script.js
+++ 12-web-browser-apis/14-speech-synthesis/script.js
@@ -1,1 +1,20 @@
 // Speech Synthesis
+// - used to type text through voice
+
+const synth = window.speechSynthesis;
+
+
+function onSubmit(e) {
+  e.preventDefault();
+  const textInput = document.getElementById('text-input');
+  
+  const utterThis = new SpeechSynthesisUtterance(textInput.value);
+  
+  synth.speak(utterThis); // Get synth to speak the text input 
+}
+
+document.getElementById('form').addEventListener('submit', onSubmit);
+
+
+
+

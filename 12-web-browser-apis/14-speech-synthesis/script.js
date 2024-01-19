fa8ca08f54b7cd2f651b5e18b4362d73c6d4388b
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-19 16:51:06 +0100

    added speech synthesis explorations

--- 12-web-browser-apis/14-speech-synthesis/script.js
+++ 12-web-browser-apis/14-speech-synthesis/script.js
@@ -1,8 +1,28 @@
 // Speech Synthesis
 // - used to type text through voice
 
-const synth = window.speechSynthesis;
 
+const voiceSelect = document.getElementById('voice-select');
+const synth = window.speechSynthesis;
+let voices;
+
+// available voices differ per operating system
+function addVoicesToSelect() {
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length; i++){
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} - ${voices[i].lang}l`;
+
+    if (voices[i].default) {
+      option.textContent += ' - default';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
 
 function onSubmit(e) {
   e.preventDefault();
@@ -10,7 +30,22 @@ function onSubmit(e) {
   
   const utterThis = new SpeechSynthesisUtterance(textInput.value);
   
+  // use the selected voice
+  const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+  for (let i  = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+
   synth.speak(utterThis); // Get synth to speak the text input 
+}
+
+addVoicesToSelect();
+
+// If the voices changed, repopulate the select
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = addVoicesToSelect;
 }
 
 document.getElementById('form').addEventListener('submit', onSubmit);

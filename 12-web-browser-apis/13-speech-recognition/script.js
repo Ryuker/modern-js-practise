847587291137562b407ca7bb1f96ae4bb5c7cf42
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-19 16:14:38 +0100

    added speech recognition api exploration

--- 12-web-browser-apis/13-speech-recognition/script.js
+++ 12-web-browser-apis/13-speech-recognition/script.js
@@ -1,1 +1,44 @@
-// Speech Recognition API
+// Speech Synthesis API 
+// ref: https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API
+
+// to ensure it works on every browser
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const rec = new SpeechRecognition();
+
+rec.lang = 'en-US';
+rec.continuous = true;   // false: listens and stops, true: it keeps listening
+
+
+rec.onresult = function(e) {
+  const acceptedColors = [
+    'red',
+    'blue',
+    'green',
+    'yellow',
+    'pink',
+    'brown',
+    'purple',
+    'orange',
+    'black',
+    'white'
+  ];
+
+  for (let i = e.resultIndex; i < e.results.length; i++) {
+    const script = e.results[i][0].transcript.toLowerCase().trim();
+    
+    if (acceptedColors.includes(script)) {
+      document.body.style.backgroundColor = script;
+    } else {
+      alert('Please say a color');
+    }
+  }
+};
+
+rec.start();
+
+
+
+
+
+

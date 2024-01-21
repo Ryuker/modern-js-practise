02d68596032a5207e9628b6608d025aa097368f1
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-21 20:44:11 +0100

    added Object Literals and This keyword exploration

--- 13-oop-constructors-prototypes/01-object-literals-this/script.js
+++ 13-oop-constructors-prototypes/01-object-literals-this/script.js
@@ -1,1 +1,26 @@
 // Object Literals & This Keyword
+const rectangle = {
+  name: 'Rectangle 1',
+  width: 10,
+  height: 10,
+  area: function() { 
+    // '.this' refers to the object this function is called in
+    return this.width * this.height; 
+  }
+};
+
+const rectangle2 = {
+  name: 'Rectangle 2',
+  width: 30,
+  height: 20,
+  area: function() { 
+    // '.this' refers to the object this function is called in
+    return this.width * this.height; 
+  }
+};
+
+
+// The weakness of the above approach is that we need to declare a new object literal for each rectangle. For large quantities this approach doesn't scale well. 
+
+
+console.log(rectangle2.area());

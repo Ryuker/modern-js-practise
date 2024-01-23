e34e4c130c791c0f7271e94d438b445c67977e35
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-23 15:38:35 +0100

    added bind() and this exploration and explanation

--- 14-oop-classes-private-properties/04-bind-this/script.js
+++ 14-oop-classes-private-properties/04-bind-this/script.js
@@ -1,1 +1,24 @@
 // Bind() and Defining this
+class App {
+  constructor () {
+    this.serverName = 'localhost';
+
+    // document
+    //   .querySelector('button')
+    //   .addEventListener('click', this.getServerName); 
+      // this points to the element it's on (in this case the button)
+
+      document
+      .querySelector('button')
+      .addEventListener('click', this.getServerName.bind(this));
+      // This binds it to call the method on the class instead instead of calling the method on the element (which doesn't have the class method)
+    
+  }
+
+  getServerName() {
+    console.log(this);
+  }
+}
+
+const app = new App();
+app.getServerName();

86ace2fae1817876d5fef54ed2930a5a177ae1a2
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-23 15:19:03 +0100

    added static method exploration and explanation

--- 14-oop-classes-private-properties/03-static-methods/script.js
+++ 14-oop-classes-private-properties/03-static-methods/script.js
@@ -1,1 +1,23 @@
 // Static Methods
+class Rectangle {
+  constructor(name, width, height) {
+    this.name = name;
+    this.width = width;
+    this.height = height;
+  }
+
+  area() {
+    return this.width * this.height;
+  }
+
+  static getClass() {
+    return 'Rectangle';
+  }
+}
+
+const rect = new Rectangle('Rect1', 20, 20);
+console.log(rect);
+console.log(rect.area());
+
+// Call a static method on a class, note you don't call it on the instance
+console.log(Rectangle.getClass());  

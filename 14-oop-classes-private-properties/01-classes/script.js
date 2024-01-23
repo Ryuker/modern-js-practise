320b6d45a4dbd2b33fdf15eea346fc26bfbeb477
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-23 14:33:21 +0100

    added class declaration exploration

--- 14-oop-classes-private-properties/01-classes/script.js
+++ 14-oop-classes-private-properties/01-classes/script.js
@@ -1,1 +1,34 @@
 // Classes Intro
+class Rectangle {
+  constructor(name, width, height) {
+    this.name = name;
+    this.width = width;
+    this.height = height;
+  }
+
+  area() {    // class methods don't use the 'function' keyword
+    return this.height * this.width;
+  }
+
+  perimeter() {
+    return 2 * (this.width * this.height);
+  }
+
+  isSquare() {
+    return this.width === this.height;
+  }
+
+  logArea() {
+    console.log('Rectangle Area: ' + this.area());
+  }
+}
+
+
+const square = new Rectangle('Square', 20, 20);
+console.log(square);
+console.log(square.area());
+console.log(square.perimeter());
+console.log(square.isSquare());
+square.logArea();
+
+console.log(Object.getPrototypeOf(square));

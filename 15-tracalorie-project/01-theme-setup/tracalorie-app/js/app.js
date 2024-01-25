0fd2f6b1fddc92e2745f6fc34f285c8f33cd19c0
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-25 16:26:20 +0100

    added app methods

---
+++ 15-tracalorie-project/01-theme-setup/tracalorie-app/js/app.js
@@ -0,0 +1,30 @@
+// Tracalorie App
+
+class App {
+  constructor() {
+
+  }
+
+  _newItem() {
+    console.log('new item called');
+  }
+
+  _removeItem() {
+    console.log('remove item called');
+  }
+  
+  _filterItems() {
+    console.log('filter item called');
+  }
+  
+  _reset() {
+    console.log('reset called');
+  }
+
+  _setLimit() {
+    console.log('set limit called');
+  }
+}
+
+
+// Methods

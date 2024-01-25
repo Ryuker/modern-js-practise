309aed05de197bdd6792d7cc7d141ba29b5aa97f
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-25 16:57:30 +0100

    added class skeletons

--- 15-tracalorie-project/01-theme-setup/tracalorie-app/js/app.js
+++ 15-tracalorie-project/01-theme-setup/tracalorie-app/js/app.js
@@ -1,5 +1,6 @@
 // Tracalorie App
 
+// The App
 class App {
   constructor() {
 
@@ -26,5 +27,126 @@ class App {
   }
 }
 
+// Meal
+class Meal {
+  constructor() {
+    this._id;
+    this._name;
+    this._calories;
+  }
+}
 
-// Methods
+// Workout
+class Workout {
+  constructor() {
+    this._id;
+    this._name;
+    this._calories;
+  }
+}
+
+
+
+// Calorie Tracker
+class CalorieTracker {
+  constructor() {
+    _calorieLimit;
+    _totalCalories;
+    _meals;
+    _workouts;
+  }
+
+  // Public methods
+  addMeal() {
+    console.log('Adding meal');
+  }
+
+  removeMeal() {
+    console.log('Removing meal');
+  }
+
+  addWorkout() {
+    console.log('Adding workout');
+  }
+
+  removeWorkout() {
+    console.log('Removing workout');
+  }
+
+  resetDay() {
+    console.log('Resetting day');
+  }
+
+  // Private methods
+  _displayCaloriesTotoal() {
+    console.log('displaying calories total')
+  }
+
+  _displayCaloryLimit() {
+    console.log('displaying calory limit')
+  }
+
+  _displayCaloriesConsumed() {
+    console.log('display calories consumed')
+  }
+
+  _displayCaloriesBurned() {
+    console.log('display calories burned')
+  }
+
+  _displayCaloriesBurned() {
+    console.log('display calories burned')
+  }
+
+  _displayCaloriesRemaining() {
+    console.log('display calories remaining')
+  }
+}
+
+// Static Storage
+class Storage{
+  static getCalorieLimit() {
+    console.log('Getting calorie limit');
+  }
+  
+  static setCalorieLimit() {
+    console.log('Setting calorie limit');
+  }
+
+  static getTotalCalories() {
+    console.log('Getting total calories');
+  }
+
+  static setTotalCalories() {
+    console.log('Setting total calories');
+  }
+
+  static updateCalories() {
+    console.log('Updating calories');
+  }
+
+  static saveMeal() {
+    console.log('Saving meal');
+  }
+
+  static removeMeal() {
+    console.log('Removing meal');
+  }
+
+  static saveWorkout() {
+    console.log('Saving workout');
+  }
+
+  static removeWorkout() {
+    console.log('Removing workout');
+  }
+
+  static getMeals() {
+    console.log('Getting meals');
+  }
+
+  static getWorkouts() {
+    console.log('Getting workouts');
+  }
+}
+

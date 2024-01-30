43bb0c4bbada5bb610ff512f650002599d9ddc0a
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-30 15:45:36 +0100

    added persistence of localCalories in localStorage

--- 15-tracalorie-project/01-theme-setup/tracalorie-app/js/app.js
+++ 15-tracalorie-project/01-theme-setup/tracalorie-app/js/app.js
@@ -148,7 +148,7 @@ class Workout {
 class CalorieTracker {
   constructor() {
     this._calorieLimit = Storage.getCalorieLimit();
-    this._totalCalories = 0;
+    this._totalCalories = Storage.getTotalCalories();
     this._meals = [];
     this._workouts = [];
 
@@ -166,6 +166,7 @@ class CalorieTracker {
     console.log('Adding meal');
     this._meals.push(meal);
     this._totalCalories += meal.calories;
+    Storage.updateTotalCalories(this._totalCalories);
     this._displayNewMeal(meal);
     this._renderStats();    
   }
@@ -177,6 +178,7 @@ class CalorieTracker {
     if (index !== -1) {
       const meal = this._meals[index];
       this._totalCalories -= meal.calories;
+      Storage.updateTotalCalories(this._totalCalories);
       this._meals.splice(index, 1);
       this._renderStats();
     }
@@ -186,6 +188,7 @@ class CalorieTracker {
     console.log('Adding workout');
     this._workouts.push(workout);
     this._totalCalories -= workout.calories;
+    Storage.updateTotalCalories(this._totalCalories);
     this._displayNewWorkout(workout);
     this._renderStats();
   }
@@ -197,6 +200,7 @@ class CalorieTracker {
     if (index !== -1) {
       const workout = this._workouts[index];
       this._totalCalories += workout.calories;
+      Storage.updateTotalCalories(this._totalCalories);
       this._workouts.splice(index, 1);
       this._renderStats();
     }
@@ -348,7 +352,7 @@ class Storage{
     if (localStorage.getItem('calorieLimit') === null) {
       calorieLimit = defaultLimit;
     } else {
-      calorieLimit = localStorage.getItem('calorieLimit');
+      calorieLimit = +localStorage.getItem('calorieLimit');
     }
 
     return calorieLimit;
@@ -359,12 +363,20 @@ class Storage{
     localStorage.setItem('calorieLimit', calorieLimit);
   }
 
-  static getTotalCalories() {
+  static getTotalCalories(defaultCalories = 0) {
     console.log('Getting total calories');
+    let totalCalories;
+    if (localStorage.getItem('totalCalories') === null) {
+      totalCalories = defaultCalories;
+    } else {
+      totalCalories = +localStorage.getItem('totalCalories');
+    }
+    return totalCalories;
   }
 
-  static setTotalCalories() {
+  static updateTotalCalories(calories) {
     console.log('Setting total calories');
+    localStorage.setItem('totalCalories', calories);
   }
 
   static updateCalories() {

7075bdbdabe6fd9aef1d573eb8601734887f1fe2
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-31 15:41:18 +0100

    added node js intro exploration

--- 16-modules-and-tooling/01-installing-using-nodejs /node-playground/app.js
+++ 16-modules-and-tooling/01-installing-using-nodejs /node-playground/app.js
@@ -1,1 +1,10 @@
 // Node Playground
+console.log('Hello from Node.js');
+
+async function getUser() {
+  const response = await fetch('https://api.github.com/users/bradtraversy'); 
+  const data = await response.json();
+  console.log(data);
+}
+
+getUser();

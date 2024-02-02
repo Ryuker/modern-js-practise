6198983f765d2f7aa9586758eca8298ca32ac39b
ryuker <lifeisagamenotaproduct@gmail.com>
2024-02-02 16:47:32 +0100

    copied over from tracalorie project: css and webfonts, copied over index.html, removed some imports in index.html

--- 16-modules-and-tooling/11-tracalorie-webpack-refactor/tracalorie-webpack/src/index.js
+++
@@ -1,6 +0,0 @@
-import message from "./message";
-import styles from "./styles/style.css";
-
-console.log(message.text);
-
-const hello = () => console.log('Hello');
--- 16-modules-and-tooling/11-tracalorie-webpack-refactor/tracalorie-webpack/src/message.js
+++
@@ -1,6 +0,0 @@
-const message = {
-  id: 1,
-  text: 'Hello this is a message'
-}
-
-export default message;

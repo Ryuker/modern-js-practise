7e86a540afdac6622511983f968d9abc73f1314f
ryuker <lifeisagamenotaproduct@gmail.com>
2024-02-12 15:12:08 +0100

    added video solutions

--- 18-unit-testing-algorithms/04-palindrome/algo-testing/palindrome/palindrome.js
+++ 18-unit-testing-algorithms/04-palindrome/algo-testing/palindrome/palindrome.js
@@ -1,7 +1,22 @@
 const reverseString = require('../reversestring/reversestring');
 
+// My solution
+// function palindrome(str) {
+//   return str == reverseString(str);
+// }
+
+// Video Solution
+// Solution 1
 function palindrome(str) {
-  return str == reverseString(str);
+  const reversed = str.split('').reverse().join('');
+  return str === reversed;
 }
+
+// Solution 2 - checks every char if it's the same as index from the opposite end of the array
+// function palindrome(str) {
+//   return str.split('').every((char, index) => {
+//     return char === str[str.length - index -1];
+//   });
+// }
 
 module.exports = palindrome;

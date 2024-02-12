75d6dd8124e4cf7eeadf394d5c82cb9917e8f975
ryuker <lifeisagamenotaproduct@gmail.com>
2024-02-12 14:33:23 +0100

    added more video solutions

--- 18-unit-testing-algorithms/03-reverse-string/reversestring/reversestring.js
+++ 18-unit-testing-algorithms/03-reverse-string/reversestring/reversestring.js
@@ -6,12 +6,31 @@ function reverseString(str = 'bla') {
 };
 
 // Video Solutions
-function reverseString2(str) {
-  return str.split('').reverse().join('');
-}
 
-function reverseString3(str) {
-  // return [...].
-}
+// Solution 1 - split version
+// function reverseString2(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// Solution 2 - For-of version
+// function reverseString(str) {
+//   let reversed = '';
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return reversed;
+// }
+
+// Solution 3 - reduce - we add the next character in front of the preceding one, reversing the order
+// function reverseString(str) {
+//   return str.split('').reduce((reversed, char) => {
+//     // console.log(char, ':char ', reversed,':reversed:' );
+//     return char + reversed
+//   }, '');
+// }
+
+// function reverseString(str) {
+//   return str.split('').reduce((reversed, char) => char + reversed,'');
+// }
 
 module.exports = reverseString;

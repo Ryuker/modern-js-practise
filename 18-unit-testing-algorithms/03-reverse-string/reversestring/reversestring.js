fe00b26621ac1be813d2ee636f2c58b42213eec3
ryuker <lifeisagamenotaproduct@gmail.com>
2024-02-12 14:13:43 +0100

    added video solution 1, refactored my solution (shortened it)

--- 18-unit-testing-algorithms/03-reverse-string/reversestring/reversestring.js
+++ 18-unit-testing-algorithms/03-reverse-string/reversestring/reversestring.js
@@ -1,6 +1,17 @@
 // Reverse String
+
+// my method
 function reverseString(str = 'bla') {
-  return newStr = [...str].reverse().join('').toString();
+  return [...str].reverse().join('');
 };
+
+// Video Solutions
+function reverseString2(str) {
+  return str.split('').reverse().join('');
+}
+
+function reverseString3(str) {
+  // return [...].
+}
 
 module.exports = reverseString;

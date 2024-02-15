0d7b39bc6a1eb3f66d80f032eac2b218f5741556
ryuker <lifeisagamenotaproduct@gmail.com>
2024-02-15 17:00:08 +0100

    added comment to http-plain

--- 19-nodejs-modules/05-http-module/http-plain.js
+++ 19-nodejs-modules/05-http-module/http-plain.js
@@ -1,10 +1,12 @@
 // Http server
 const http = require('http');
 
+// Create the server
 const server = http.createServer((request, response) => {
   response.end('Hello World');
 });
 
+// Run the server
 server.listen(5000, () => {
   console.log('Server is listening on port 5000');
 });

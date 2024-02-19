f7704bb457b6db8f6da1e6dfa9581a7192c88a42
ryuker <lifeisagamenotaproduct@gmail.com>
2024-02-19 15:09:34 +0100

    added functionality to return ideas and a single idea based on the id

---
+++ 20-randomideas-rest-api/rest-api.md
@@ -0,0 +1,35 @@
+# Rest API using Express
+
+## Intro to Express
+install:
+``` JS Terminal
+$npm i express
+```
+
+basic server setup:
+``` JS server.js
+const express = require('express');
+const port = 5000;
+
+// Initialize a new Express app
+const app = express();
+
+// Get request for '/' route
+app.get('/', (req, res) => { 
+  res.send('Hello World');
+});
+
+// start the server
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+```
+
+returning a single item from a dynamic route:
+- use the ':' in the url
+- use req.params.id to get the param
+``` JS
+// Get a single idea
+app.get('/api/ideas/:id', (req, res) => { 
+  req.params.id;
+  res.send({ success: true, data: req.params.id });
+});
+```
--- 20-randomideas-rest-api/server.js
+++ 20-randomideas-rest-api/server.js
@@ -4,9 +4,48 @@ const port = 5000;
 // Initialize a new Express app
 const app = express();
 
+const ideas = [
+  {
+    id: 1,
+    text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
+    tag: 'Technology',
+    username: 'TonyStark',
+    date: '2022-01-02',
+  },
+  {
+    id: 2,
+    text: 'Milk cartons that turn a different color the older that your milk is getting',
+    tag: 'Inventions',
+    username: 'SteveRogers',
+    date: '2022-01-02',
+  },
+  {
+    id: 3,
+    text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
+    tag: 'Software',
+    username: 'BruceBanner',
+    date: '2022-01-02',
+  },
+];
+
 // Get request for '/' route
 app.get('/', (req, res) => { 
-  res.send('Hello World');
+  res.send({message: 'Welcome to the RandomIdeas API'});
+});
+
+// Get all ideas
+app.get('/api/ideas', (req, res) => { 
+  res.send({ success: true, data: ideas });
+});
+
+// Get a single idea
+app.get('/api/ideas/:id', (req, res) => { 
+  const idea = ideas.find(ideas => ideas.id === +req.params.id);
+  if (!idea){
+    res.status(404).json({ success: false , error: 'Resource not found' });
+  } else {
+    res.json({ success: true, data: idea });
+  }
 });
 
 // start the server

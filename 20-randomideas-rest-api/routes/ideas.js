c7c222db1f4939e2b47f5cd14e2d4e3f292b3207
ryuker <lifeisagamenotaproduct@gmail.com>
2024-02-21 16:26:13 +0100

    added delete request handling

--- 20-randomideas-rest-api/routes/ideas.js
+++ 20-randomideas-rest-api/routes/ideas.js
@@ -72,7 +72,19 @@ router.put('/:id', (req, res) => {
   res.json({ succes: true, data: idea});
 });
 
-// 
+// Delete - Remove an idea from the array
+router.delete('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);  // Reference to the object inside the array
+
+  if (!idea){
+    return res.status(404).json({ success: false , error: 'Resource not found' });
+  }
+
+  const ideaIndex = ideas.indexOf(idea);
+  ideas.splice(ideaIndex, 1);
+
+  res.json({ success: true, data: {} })
+});
 
 
 module.exports = router;

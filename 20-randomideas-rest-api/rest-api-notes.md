ad095e93b55e85bfee7be175f18c841005e09991
ryuker <lifeisagamenotaproduct@gmail.com>
2024-02-21 13:54:16 +0100

    moved ideas routes into its own router file

--- 20-randomideas-rest-api/rest-api-notes.md
+++ 20-randomideas-rest-api/rest-api-notes.md
@@ -17,3 +17,31 @@ $NPM i -D nodemon
 "dev": "nodemon server.js"
 ``` 
 
+## Routes organization
+- You can add routes in a different file.
+- To do so you need to import the express router and export it.
+
+Example:
+``` JS routes.js
+const express = require('express');
+const router = express.Router();
+
+~~~ Routes ~~~
+router.get('/', (req, res) => { 
+  res.send({ success: true, data: ideas });
+});
+
+etc
+~~~~
+
+module.exports = router;
+```
+
+importing the router into the server file
+- The router is middleware, so we use 'app.use()' to use it.
+``` JS server.js
+// import the router
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter);  // middleware
+```
+
--- 20-randomideas-rest-api/server.js
+++ 20-randomideas-rest-api/server.js
@@ -4,49 +4,14 @@ const port = 5000;
 // Initialize a new Express app
 const app = express();
 
-const ideas = [
-  {
-    id: 1,
-    text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
-    tag: 'Technology',
-    username: 'TonyStark',
-    date: '2022-01-02',
-  },
-  {
-    id: 2,
-    text: 'Milk cartons that turn a different color the older that your milk is getting',
-    tag: 'Inventions',
-    username: 'SteveRogers',
-    date: '2022-01-02',
-  },
-  {
-    id: 3,
-    text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
-    tag: 'Software',
-    username: 'BruceBanner',
-    date: '2022-01-02',
-  },
-];
-
 // Get request for '/' route
 app.get('/', (req, res) => { 
   res.send({message: 'Welcome to the RandomIdeas API'});
 });
 
-// Get all ideas
-app.get('/api/ideas', (req, res) => { 
-  res.send({ success: true, data: ideas });
-});
-
-// Get a single idea
-app.get('/api/ideas/:id', (req, res) => { 
-  const idea = ideas.find(ideas => ideas.id === +req.params.id);
-  if (!idea){
-    return res.status(404).json({ success: false , error: 'Resource not found' });
-  }
-
-  res.json({ success: true, data: idea });
-});
+// import the router
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter);
 
 // start the server
 app.listen(port, () => console.log(`Server listening on port ${port}`));
